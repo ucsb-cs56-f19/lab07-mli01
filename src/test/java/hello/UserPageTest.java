9e77783a119d33982fe387319b6af216a10cf38e
Matthew Li <mli01@umail.ucsb.edu>
2019-12-03 00:41:58 -0800

    ml - added User nav bar test in UserPageTest.java

---
+++ src/test/java/hello/UserPageTest.java
@@ -0,0 +1,55 @@
+package hello;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(WebController.class)
+public class UserPageTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+	@MockBean
+	private AuthControllerAdvice aca;
+
+	@MockBean
+	private ClientRegistrationRepository crr;
+
+    @Test
+    public void navbar_has_userlink() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").string("Users"));
+    }
+
+    /*
+    @Test
+    public void navbar_userlink_links_correct() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").attr("href").equals("/users"));
+    }
+    */
+
+
+}
